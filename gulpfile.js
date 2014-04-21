47750bb20415a672b4403d83703df08909664586
Ryan Pineo <ryanpineo@gmail.com>
2014-04-21 11:37:09 -0400

    Update bower deps.  Load libs explicity in gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -2,57 +2,62 @@ var gulp = require('gulp');
 var plugins = require("gulp-load-plugins")({lazy:false});
 
 gulp.task('scripts', function(){
-    //combine all js files of the app
-    gulp.src(['!./app/**/*_test.js','./app/**/*.js'])
-        .pipe(plugins.jshint())
-        .pipe(plugins.jshint.reporter('default'))
-        .pipe(plugins.concat('app.js'))
-        .pipe(gulp.dest('./build'));
+  //combine all js files of the app
+  gulp.src(['!./app/**/*_test.js','./app/**/*.js'])
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('default'))
+    .pipe(plugins.concat('app.js'))
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('templates',function(){
-    //combine all template files of the app into a js file
-    gulp.src(['!./app/index.html',
-        './app/**/*.html'])
-        .pipe(plugins.angularTemplatecache('templates.js',{standalone:true}))
-        .pipe(gulp.dest('./build'));
+  //combine all template files of the app into a js file
+  gulp.src(['!./app/index.html', './app/**/*.html'])
+    .pipe(plugins.angularTemplatecache('templates.js',{standalone:true}))
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('css', function(){
-    gulp.src('./app/**/*.css')
-        .pipe(plugins.concat('app.css'))
-        .pipe(gulp.dest('./build'));
+  gulp.src('./app/**/*.css')
+    .pipe(plugins.concat('app.css'))
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('vendorJS', function(){
-    //concatenate vendor JS files
-    gulp.src(['!./bower_components/**/*.min.js',
-        './bower_components/**/*.js'])
-        .pipe(plugins.concat('lib.js'))
-        .pipe(gulp.dest('./build'));
+  var V = function(p){return './bower_components/' + p;};
+  //concatenate vendor JS files
+  gulp.src([
+    V('angular/angular.js'),
+    V('angular-mocks/angular-mocks.js'),
+    V('angular-ui-router/release/angular-ui-router.js'),
+    V('lodash/dist/lodash.js'),
+    V('restangular/dist/restangular.js')
+  ])
+    .pipe(plugins.concat('lib.js'))
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('vendorCSS', function(){
-    //concatenate vendor CSS files
-    gulp.src(['!./bower_components/**/*.min.css',
-        './bower_components/**/*.css'])
-        .pipe(plugins.concat('lib.css'))
-        .pipe(gulp.dest('./build'));
+  //concatenate vendor CSS files
+  gulp.src(['!./bower_components/**/*.min.css',
+    './bower_components/**/*.css'])
+    .pipe(plugins.concat('lib.css'))
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('copy-index', function() {
-    gulp.src('./app/index.html')    
-        .pipe(gulp.dest('./build'));
+  gulp.src('./app/index.html')
+    .pipe(gulp.dest('./build'));
 });
 
 gulp.task('watch',function(){
-    gulp.watch([
-        'build/**/*.html',        
-        'build/**/*.js',
-        'build/**/*.css'        
+  gulp.watch([
+    'build/**/*.html',
+    'build/**/*.js',
+    'build/**/*.css'
     ], function(event) {
-        return gulp.src(event.path)
-            .pipe(plugins.connect.reload());
+      return gulp.src(event.path)
+      .pipe(plugins.connect.reload());
     });
     gulp.watch(['./app/**/*.js','!./app/**/*test.js'],['scripts']);
     gulp.watch(['!./app/index.html','./app/**/*.html'],['templates']);
@@ -62,9 +67,9 @@ gulp.task('watch',function(){
 });
 
 gulp.task('connect', plugins.connect.server({
-    root: ['build'],
-    port: 9000,
-    livereload: true
+  root: ['build'],
+  port: 9000,
+  livereload: true
 }));
 
 gulp.task('default',['connect','scripts','templates','css','copy-index','vendorJS','vendorCSS','watch']);
